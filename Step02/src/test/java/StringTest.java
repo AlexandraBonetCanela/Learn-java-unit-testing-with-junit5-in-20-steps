6c4340fdb76692a9fa0f6f7e3ae08e04eb4a8bde
Alex Bonet Canela <bonet.canela.alex@gmail.com>
2024-11-05 15:29:53 +0100

    Added tests for testing string

--- Step02/src/test/java/StringTest.java
+++ Step02/src/test/java/StringTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class StringTest {
@@ -7,5 +8,19 @@ public class StringTest {
     @Test
     void test(){
         fail("Not yet implemented");
+    }
+
+    @Test
+    void stringPassTest(){
+        int actualLength = "ABCD".length();
+        int expectedLength = 4;
+        assertEquals(actualLength,expectedLength);
+    }
+
+    @Test
+    void stringFailsTest(){
+        int actualLength = "ABCDE".length();
+        int expectedLength = 4;
+        assertEquals(actualLength,expectedLength);
     }
 }
